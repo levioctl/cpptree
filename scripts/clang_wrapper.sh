8f268da965e067eb7b5109514b9b2464c4d6a8c8
Eliran Levi <levioctl@gmail.com>
2018-04-11 18:16:59 +0300

    Wrap the clang command line in order to generate compilation database during compilation

---
+++ scripts/clang_wrapper.sh
@@ -0,0 +1,17 @@
+# The purpose of this script is to wrap the clang compiler command so that it can be
+# used by autotools in such a way that passes unique flag per file.
+#
+# Specifically, the "-MJ" flag of clang is needed to create the compilation database
+# JSON file, but this flag requires an output filename, so it can't simply be put
+# AM_CXXFLAGS in the Makefile.am (as an additional compilation flag), since a unique
+# (per-file) flag cannot be specified in Makefile.am's AM_CXXFLAGS (for instance,
+# specifying '-MJ bla.compilation-database.json' in the command that compiles bla.cpp).
+# For this purpose, this script uses as a wrapper for the compiler; It intercepts the
+# target object file and adds the "-MJ" flag with a json filename that is unique
+# for the object file that is being built in that command.
+
+# Find the last flag
+for objectfile_name; do true; done
+
+# Run the compilation with the compilation database flag
+clang $@ -MJ $objectfile_name.compilation-database.json
